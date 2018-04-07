219cc1e287f627f34841e88d1681cde776b2e4b8
Mirawati54 <fti.mirawati@gmail.com>
2018-04-07 12:33:39 +0800

    Membuat Atribut dan Behavior

---
+++ src/praktek01/Praktek01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01;
+
+/**
+ *
+ * @author ACER
+ */
+public class Praktek01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/praktek01/penjualan_kompor_gas.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01;
+
+/**
+ *
+ * @author ACER
+ */
+public class penjualan_kompor_gas {
+    
+}
